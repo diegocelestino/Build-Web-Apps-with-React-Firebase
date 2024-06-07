53bce45309d1f56b8ad0fcf92b52db5d00c1bb5e
Diego <diegocdsantos@gmail.com>
2024-06-06 23:12:30 -0300

    Router project did

---
+++ multipage-site/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { BrowserRouter, Route, Routes, Link, NavLink, Navigate } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Article from './pages/Article';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <nav>
+        <h1>My Articles</h1>
+        <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+      </nav>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/articles/:id' element={<Article />} />
+          <Route path='*' element={<Navigate to="/"/>}/>
+        </Routes>
+      
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
