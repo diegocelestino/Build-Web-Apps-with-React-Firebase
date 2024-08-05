3064ca02c7d9fbf20da9269a4c72c78d020702a6
Diego <diegocdsantos@gmail.com>
2024-08-05 10:28:21 -0300

    added firestore rules

---
+++ the-dojo/src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8AuBrqC1RS55RjmO0aT_xJJmgNlIfp8s",
+    authDomain: "thedojosite-a0f93.firebaseapp.com",
+    projectId: "thedojosite-a0f93",
+    storageBucket: "thedojosite-a0f93.appspot.com",
+    messagingSenderId: "1096563801044",
+    appId: "1:1096563801044:web:1936459c6d843760577441"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
